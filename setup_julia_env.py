f53d0f42c01335b1080e5ad1d8196ad0a09f9d57
Mark Kittisopikul <kittisopikulm@janelia.hhmi.org>
2025-09-22 16:34:20 -0400

    Use ShroffCelegansModelsCore.jl

--- setup_julia_env.py
+++ setup_julia_env.py
@@ -1,5 +1,3 @@
 import juliapkg
-juliapkg.add("ThinPlateSplines", "1d861738-f48e-4029-b1d3-81ce6bc7f5ab", url="https://github.com/mkitti/ThinPlateSplines.jl", rev="mkitti-type-stability")
-juliapkg.add("ShroffCelegansModels","28a312d2-d9d3-46a7-98c1-9c09f12e8c99",url="https://github.com/JaneliaSciComp/ShroffCelegansModels.jl", dev=True)
-juliapkg.resolve()
+juliapkg.add("ShroffCelegansModelsCore", uuid="e23caf7b-3362-4cb0-a14a-545330a3115b", url="https://github.com/JaneliaSciComp/ShroffCelegansModels.jl", subdir="ShroffCelegansModelsCore", rev="core")
 print("Julia environment configured")
